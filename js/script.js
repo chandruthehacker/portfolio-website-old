142e392907b9b8cbdf32534ebe4f45386843ec5b
Chandru <112714745+chandruthehacker@users.noreply.github.com>
2025-04-25 15:52:37 +0530

    Update script.js fix navbar click problem

--- js/script.js
+++ js/script.js
@@ -4,35 +4,26 @@ let navbar = document.querySelector(".navbar");
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
-
-  if (navbar.classList.contains("active")) {
-    navbar.style.visibility = "visible";
-    navbar.style.display = "block";
-  } else {
-    setTimeout(() => {
-      navbar.style.visibility = "hidden";
-      navbar.style.display = "none";
-    }, 500);
-  }
 };
 
 document.addEventListener("click", (event) => {
   const isClickInsideMenu =
     navbar.contains(event.target) || menuIcon.contains(event.target);
+
   if (navbar.classList.contains("active") && !isClickInsideMenu) {
     menuIcon.classList.remove("bx-x");
     navbar.classList.remove("active");
-    setTimeout(() => {
-      navbar.style.visibility = "hidden";
-      navbar.style.display = "none";
-    }, 500);
   }
 });
 
+// Scroll behavior
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
   sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
@@ -40,30 +31,28 @@ window.onscroll = () => {
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
+      navLinks.forEach((link) => link.classList.remove("active"));
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
     }
   });
-  //sticky navbar
-  let header = document.querySelector("header");
 
-  header.classList.toggle("sticky", window.scrollY > 100);
-
+  // close navbar on scroll
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
 
-ScrollReveal({
-  reset: true,
-  distance: "80px",
-  duration: 2000,
-  delay: 100,
+// smooth close on link click
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+  });
 });
 
+
+// scroll reveal contents
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(".home-image, .contact form", {
   origin: "bottom",
